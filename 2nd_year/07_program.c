0b8abc6b6d7c04eef73725075742a55e6182d352
Irfan <Irfan@sk_mahammad_Irfan>
2022-07-18 20:54:22 +0530

    .

---
+++ 2nd_year/07_program.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int i;
+    int a[6] = {12, 3, 4, 6, 1, 8};
+    int b[6] = {8, 1, 6, 4, 3, 12};
+    int c[6];
+    for (i = 0; i < 6; i++)
+    {
+        c[i] = a[i] + b[i];
+        printf("%d ", c[i]);
+    }
+    return 0;
+}
--- 2nd_year/08_program.c
+++ 2nd_year/08_program.c
@@ -1,14 +1,30 @@
 #include <stdio.h>
 int main()
 {
-    int i;
-    int a[6] = {12, 3, 4, 6, 1, 8};
-    int b[6] = {8, 1, 6, 4, 3, 12};
-    int c[6];
-    for (i = 0; i < 6; i++)
+    int a[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}, i, j, m;
+    for (i = 0; i < 3; i++)
     {
-        c[i] = a[i] + b[i];
-        printf("%d ", c[i]);
+        for (j = 0; j < 3; j++)
+        {
+            printf("%d", a[i][j]);
+        }
+        printf("\n");
     }
+    printf("\n");
+    for (i = 0; i < 3; i++)
+    {
+        m = a[i][0];
+        a[i][0] = a[i][2];
+        a[i][2] = m;
+    }
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            printf("%d", a[i][j]);
+        }
+        printf("\n");
+    }
+
     return 0;
 }
